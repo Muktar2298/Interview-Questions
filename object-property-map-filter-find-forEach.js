d34dd01a891225a604193fdebdc3ca3b57bbf18c
Muktar2298 <muktar15-2298@diu.edu.bd>
2021-07-10 02:45:30 +0600

    interview-questions

---
+++ object-property-map-filter-find-forEach.js
@@ -0,0 +1,9 @@
+const students = [
+  { id: 1, name: "nishat" },
+  { id: 2, name: "hasan" },
+  { id: 3, name: "muktar" },
+];
+// const result = students.map((element) => element.name);
+// const result = students.filter(element => element.id > 0);
+// const result = students.find((element) => element.id > 0);
+console.log(result);
