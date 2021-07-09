9d957b38f9d8bb6f30d34f9fe3b4f9485cfa2555
Muktar2298 <muktar15-2298@diu.edu.bd>
2021-07-10 00:48:29 +0600

    interview-practice

---
+++ undefinedVsnull.js
@@ -0,0 +1,40 @@
+                                                                      // How we get undefined 
+// 1.
+// let num;
+// console.log(num);
+
+//--2
+// function myFunction(x,y){
+//     const add = x + y;
+//     return add;
+// }
+// const result = myFunction(10);
+// console.log(result);
+
+//--3
+// function myFunction(x,y){
+//     const add = x + y;
+// }
+// const result = myFunction(10);
+// console.log(result);
+
+// 4.
+// function myFunction(x,y){
+//     const add = x + y;
+//     return
+// }
+// const result = myFunction(10);
+// console.log(result);
+
+// 5.
+// let name = undefined;
+// console.log(name);
+
+// 6.
+// let person = {name : 'John', age :22}
+// console.log(person.gf);
+
+
+// // 7.
+// let array = [1,2,3,4];
+// console.log(array[5]);
